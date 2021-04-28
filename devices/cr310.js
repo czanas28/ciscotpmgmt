785c90bd7189f4b93dc8a8ef27780c66c0077efe
czanas28 <60748022+czanas28@users.noreply.github.com>
2021-04-28 18:05:35 -0400

    Update cr310.js

--- devices/cr310.js
+++ devices/cr310.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'cr310',
     customUi: false,
     muteOnCallStart: false,
-    cmsAccounts: ['JuvDepDivD@fljud13.org', 'shelterhearings@fljud13.org'],
+    cmsAccounts: [],
     zoomAccounts: [],
 }

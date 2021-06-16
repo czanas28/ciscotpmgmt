94e0970e2a574bd5ffc258cc89dee7b43c8a84dc
czanas28 <60748022+czanas28@users.noreply.github.com>
2021-06-16 19:58:19 -0400

    Removed private info

--- devices/room55.js
+++ devices/room55.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'room55',
     customUi: false,
     muteOnCallStart: true,
-    cmsAccounts: ['zanasc@fljud13.org', 'shelterhearings@fljud13.org'],
+    cmsAccounts: [],
     zoomAccounts: [],
 }
